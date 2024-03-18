d5c38150ae0a31d547010d0b087603f173a6daba
LOUDO <lucaslelouer@gmail.com>
2024-03-18 10:40:41 +0100

    Ajout parametre si n'existe pas dans la table

--- Atlantik_app_admin/barre_menu/modifier/FormModifParametreSite.cs
+++ Atlantik_app_admin/barre_menu/modifier/FormModifParametreSite.cs
@@ -17,6 +17,7 @@ namespace Atlantik_app_admin.barre_menu.modifier
     {
 
         MySqlConnection conn = new MySqlConnection(BDD.CONNECTION_STRING);
+        bool isParamInsertedInDb = false; // Vérifier si il existe une ligne dans la bdd contenant les paramètres.
 
         public FormModifParametreSite()
         {
@@ -33,6 +34,7 @@ namespace Atlantik_app_admin.barre_menu.modifier
                 var param = cmd.ExecuteReader();
                 while (param.Read())
                 {
+                    isParamInsertedInDb = true;
                     tbx_site.Text = param["SITE_PB"].ToString();
                     tbx_rang.Text = param["RANG_PB"].ToString();
                     tbx_identifiant.Text = param["IDENTIFIANT_PB"].ToString();
@@ -64,29 +66,37 @@ namespace Atlantik_app_admin.barre_menu.modifier
             try
             {
                 conn.Open();
-                string req = "UPDATE parametres " +
-                "SET SITE_PB = @SITE " +
-                "WHERE NOIDENTIFIANT = 0; " +
+                string req = "";
+                if (isParamInsertedInDb)
+                {
+                    req = "UPDATE parametres " +
+                        "SET SITE_PB = @SITE " +
+                        "WHERE NOIDENTIFIANT = 0; " +
 
-                "UPDATE parametres " +
-                "SET RANG_PB = @RANG " +
-                "WHERE NOIDENTIFIANT = 0; " +
+                        "UPDATE parametres " +
+                        "SET RANG_PB = @RANG " +
+                        "WHERE NOIDENTIFIANT = 0; " +
 
-                "UPDATE parametres " +
-                "SET IDENTIFIANT_PB = @IDENTIFIANT " +
-                "WHERE NOIDENTIFIANT = 0; " +
+                        "UPDATE parametres " +
+                        "SET IDENTIFIANT_PB = @IDENTIFIANT " +
+                        "WHERE NOIDENTIFIANT = 0; " +
 
-                "UPDATE parametres " +
-                "SET CLEHMAC_PB = @CLEHMAC " +
-                "WHERE NOIDENTIFIANT = 0; " +
+                        "UPDATE parametres " +
+                        "SET CLEHMAC_PB = @CLEHMAC " +
+                        "WHERE NOIDENTIFIANT = 0; " +
 
-                "UPDATE parametres " +
-                "SET ENPRODUCTION = @ENPRODUCTION " +
-                "WHERE NOIDENTIFIANT = 0; " +
+                        "UPDATE parametres " +
+                        "SET ENPRODUCTION = @ENPRODUCTION " +
+                        "WHERE NOIDENTIFIANT = 0; " +
 
-                "UPDATE parametres " +
-                "SET MELSITE = @MEL " +
-                "WHERE NOIDENTIFIANT = 0; ";
+                        "UPDATE parametres " +
+                        "SET MELSITE = @MEL " +
+                        "WHERE NOIDENTIFIANT = 0; ";
+                } else
+                {
+                    req = "INSERT INTO parametres(SITE_PB, RANG_PB, IDENTIFIANT_PB, CLEHMAC_PB, ENPRODUCTION, MELSITE) " +
+                        "VALUES (@SITE,@RANG,@IDENTIFIANT,@CLEHMAC,@ENPRODUCTION,@MEL)";
+                }
 
                 var cmd = new MySqlCommand(req, conn);
                 cmd.Parameters.AddWithValue("@SITE", tbx_site.Text);
