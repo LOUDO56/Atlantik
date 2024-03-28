7bee30f870f0545cb6ed81c3544abed958c8f8ed
LOUDO <lucaslelouer@gmail.com>
2024-03-28 15:09:05 +0100

    Affichage tarif pour une liaison

--- app/Models/TarifLiaisonModel.php
+++ app/Models/TarifLiaisonModel.php
@@ -17,16 +17,20 @@ class TarifLiaisonModel extends Model
 
     public function getTarifParUneLiaison(int $noliaison)
     {
-        return $this->select('categorie.LIBELLE as "CATEGORIE", type.LETTRECATEGORIE as "TYPE", type.LIBELLE, DATEDEBUT, DATEFIN, TARIF')
+        return $this->select('categorie.LIBELLE as "CATEGORIE", type.LETTRECATEGORIE, type.LIBELLE, type.NOTYPE, DATEDEBUT, DATEFIN, TARIF')
             ->join('port as portdepart', 'liaison.NOPORT_DEPART = portdepart.NOPORT')
             ->join('port as portarrive', 'liaison.NOPORT_ARRIVEE = portarrive.NOPORT')
             ->join('tarifer', 'liaison.NOLIAISON = tarifer.NOLIAISON')
             ->join('periode', 'tarifer.NOPERIODE = periode.NOPERIODE')
-            ->join('type', 'tarifer.LETTRECATEGORIE = type.LETTRECATEGORIE')
+            ->join('type', 'tarifer.NOTYPE = type.NOTYPE AND tarifer.LETTRECATEGORIE = type.LETTRECATEGORIE')
             ->join('categorie', 'type.LETTRECATEGORIE = categorie.LETTRECATEGORIE')
-            ->where(['liaison.NOLIAISON' => $noliaison])
+            ->where('tarifer.NOLIAISON', $noliaison)
+            ->where('periode.DATEDEBUT <= periode.DATEFIN')
+            ->orderBy('tarifer.LETTRECATEGORIE, tarifer.NOTYPE')
             ->get()
             ->getResult();
+        // La requête de fou
+
     }
 
 }
--- app/Views/visiteur/tarif_pour_une_liaison.php
+++ app/Views/visiteur/tarif_pour_une_liaison.php
@@ -1,15 +1,23 @@
 <div class="container py-5 mt-5">
     <table class="table table-success">
         <thead>
-            <th>Catégorie</th>
-            <th>Type</th>
-            <th>Période</th>
+            <tr>
+                <th>Catégorie</th>
+                <th>Type</th>
+                <th>Periode</th>
+                <th>Tarif</th>
+            </tr>	
         </thead>
         <tbody>
+            <?php $categorie = ""?>
             <?php foreach($datas as $data):?>
                 <tr>
-                    <td><?= $data->CATEGORIE?></td>
+                    <td><?= $data->LETTRECATEGORIE . " " . $data->CATEGORIE?></td>
+                    <td><?= $data->LETTRECATEGORIE . $data->NOTYPE . " - " . $data->LIBELLE?></td>
+                    <td><?= $data->DATEDEBUT . " " . $data->DATEFIN?></td>
+                    <td><?= $data->TARIF?></td>
                 </tr>
+                <?php $categorie = $data->CATEGORIE?>
             <?php endforeach?>
         </tbody>
     </table>
